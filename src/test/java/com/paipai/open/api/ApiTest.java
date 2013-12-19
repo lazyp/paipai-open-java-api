e1672cd3f7a3273bfa96f290ef496eb86bb3a9c8
lazyp <lazy_p@163.com>
2013-12-19 12:56:16 +0800

    添加登录签名验证

--- src/test/java/com/paipai/open/api/ApiTest.java
+++ src/test/java/com/paipai/open/api/ApiTest.java
@@ -7,11 +7,31 @@ import java.util.TreeMap;
 
 import org.junit.Test;
 
+import com.paipai.open.api.utils.SignUtils;
+
 /**
  * @author <a href="mailto:lazy_p@163.com">lazyp</a>
  * @version 2013-12-18
  */
 public class ApiTest {
+    /**
+     * 登录签名验证
+     */
+    public void siginTest() {
+        String accessToken = "";
+        long useruin = 1111;// qq号
+        String appAuthId = "";// 自己应用的authid
+        String appOauthKey = "";// 自己应用的OauthKey
+        /* 签名验证 */
+        TreeMap<String, Object> params = new TreeMap<String, Object>();
+        params.put("access_token", accessToken);
+        params.put("useruin", useruin);
+        params.put("app_oauth_id", appAuthId);
+        String calSign = SignUtils.sign("", "", appOauthKey, params);// 注意登录签名验证，是没有httpMethod(请求方式GET or POST)，api
+                                                                     // path的
+        System.out.println(calSign);
+    }
+
     /**
      * 获取用户个人信息
      */
